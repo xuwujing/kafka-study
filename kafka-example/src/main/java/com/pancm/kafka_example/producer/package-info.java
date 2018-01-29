58985bb2984bda287cf754b870a2c0c111ac2775
xuwujing <1060589146@qq.com>
2018-01-29 08:26:06 +0800

    kafka提交测试

---
+++ kafka-example/src/main/java/com/pancm/kafka_example/producer/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * Title: package-info
+ * Description: 
+ * kafka 生产者程序
+ * Version:1.0.0  
+ * @author pancm
+ * @date 2018年1月28日
+ */
+package com.pancm.kafka_example.producer;
